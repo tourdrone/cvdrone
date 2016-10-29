871698f0544203e9f354635e3d1106360074bcf0
LambentLight <elliotdg@live.com>
2016-10-29 13:51:30 -0700

    Move flight movement and enum structures into separate file.

--- src/manual/manual.cpp
+++ src/manual/manual.cpp
@@ -1,6 +1,32 @@
+/*
+*/
 
 #include "manual.h"
 
+/*
+*/
+ControlMovements manualMovement(int key) {
+  ControlMovements controlMovements;
+  
+  controlMovements.vx = 0;
+  controlMovements.vy = 0;
+  controlMovements.vz = 0;
+  controlMovements.vr = 0;
+
+  if (key == 't') { controlMovements.vx =  1.0; } //t key
+  if (key == 'g') { controlMovements.vx = -1.0; } //g key
+  if (key == 'f') { controlMovements.vy =  1.0; } //f key
+  if (key == 'h') { controlMovements.vy = -1.0; } //h key
+  if (key == 'q') { controlMovements.vz =  1.0; } //q key
+  if (key == 'a') { controlMovements.vz = -1.0; } //a key
+  if (key == 'r') { controlMovements.vr =  1.0; } //r key
+  if (key == 'y') { controlMovements.vr = -1.0; } //y key
+
+  return controlMovements;
+}
+
+/*
+*/
 void displayManualInfo(cv::Mat *image, double vx, double vy, double vz, double vr) {
   cv::Scalar green = CV_RGB(0,255,0); //putText color value
 
