a09271b8e47edc55fb59621e17848c22fe46c95e
LambentLight <elliotdg@live.com>
2016-10-29 07:27:05 -0700

    Add files for separating out the object following code.
    Former-commit-id: ae0102dff53bf412acf383bc150beb43cf86d4e4 [formerly 2eabd4846ff33ee2c88fd258f727fc81d6980e22]
    Former-commit-id: e20a05f5ea6787737dda3f2341738ce5de8e8024

---
+++ src/objectFollowing/objectFollowing.h
@@ -0,0 +1,10 @@
+
+#include "../ardrone/ardrone.h"
+
+/*
+*/
+void setHSVTrackBarPositions(int hue, int saturation, int value, int tolerance);
+
+/*
+*/
+void displayObjectFollowingInfo(cv::Mat *image, double heading, int hue, int saturation, int value);
