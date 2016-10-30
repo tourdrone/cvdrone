7db0e9852b10f3e2683a1504bb270500c3e7faad
Caleb <cmennen@vols.utk.edu>
2016-10-30 13:30:22 -0400

    testing

--- src/lineFollowing/lineFollowing.cpp
+++ src/lineFollowing/lineFollowing.cpp
@@ -15,6 +15,7 @@ ControlMovements lineFollowingControl() {
 }
 
 void line_main() {
+  printf("Hello, this is Caleb");
   VideoCapture cap("videos/top_down_4.m4v");
   if (!cap.isOpened())  // check if we succeeded
     return;
