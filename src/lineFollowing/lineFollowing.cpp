2158c5c788b0807e0fac29cd4ea84f385b8c4654
Caleb <cmennen@vols.utk.edu>
2016-10-30 14:00:26 -0400

    testing lines on mask
    Former-commit-id: 0749ec5f5c2be7784855b93ff5d2f16b87620763 [formerly 07ecb08654a19e5e60e8e9dbe60b650478c7eb16]
    Former-commit-id: fdec4350142c5dba21fb08f6f29a7cd26b4b2869

--- src/lineFollowing/lineFollowing.cpp
+++ src/lineFollowing/lineFollowing.cpp
@@ -24,7 +24,7 @@ void line_main() {
   namedWindow("edges", CV_WINDOW_NORMAL);
   Mat edges;
   for (; ;) {
-    Mat mask;
+    Mat mask, hsv_mask;
     Mat frame, gray;
     cap >> frame; // get a new frame from camera
     cvtColor(frame, gray, CV_BGR2HSV);
@@ -35,18 +35,19 @@ void line_main() {
 
 
     inRange(gray, low, high, mask);
-
+//    cvtColor(mask, hsv_mask, CV_HS);
+    hsv_mask = mask;
     vector <Vec4i> lines;
     HoughLinesP(mask, lines, 1, CV_PI / 180, 100, 100, 10);
 
     printf("Adding in %d lines\n", (int) lines.size());
     for (size_t i = 0; i < lines.size(); i++) {
       Vec4i l = lines[i];
-      line(frame, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
+      line(hsv_mask, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, CV_AA);
     }
 
 
-    imshow("edges", frame);
+    imshow("edges", hsv_mask);
     if (waitKey(30) >= 0) break;
   }
   // the camera will be deinitialized automatically in VideoCapture destructor
