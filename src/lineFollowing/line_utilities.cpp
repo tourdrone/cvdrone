a67face036e3fb462fc4ce1b54c7f43e4ddef7bc
Caleb <cmennen@vols.utk.edu>
2016-11-15 18:20:38 -0500

    starting some refactoring

--- src/lineFollowing/line_utilities.cpp
+++ src/lineFollowing/line_utilities.cpp
@@ -5,8 +5,17 @@
 #include "lineFollowing.h"
 #include "line_utilities.h"
 
+
 cv::Vec2f flip_line(cv::Vec2f line) {
   line[0] -= deg2rad(180);
   line[1] *= -1;
   return line;
+}
+
+double deg2rad(double deg) {
+  return deg * (CV_PI / 180.0);
+}
+
+double rad2deg(double rad) {
+  return rad * (180.0 / CV_PI);
 }
