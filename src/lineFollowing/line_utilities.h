12a243dcef50eba0f0c6303b2ca24ff54945674e
Caleb <cmennen@vols.utk.edu>
2016-11-15 18:13:31 -0500

    starting some refactoring

---
+++ src/lineFollowing/line_utilities.h
@@ -0,0 +1,10 @@
+//
+// Created by Caleb on 11/15/16.
+//
+
+#ifndef CVDRONE_LINE_UTILITIES_H
+#define CVDRONE_LINE_UTILITIES_H
+
+cv::Vec2f flip_line(cv::Vec2f line);
+
+#endif //CVDRONE_LINE_UTILITIES_H
