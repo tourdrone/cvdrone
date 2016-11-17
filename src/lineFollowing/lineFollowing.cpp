d1d60b1db36d51b990e0aba0062e2f68997db54f
Caleb <cmennen@vols.utk.edu>
2016-11-17 12:33:14 -0500

    code reorganization

--- src/lineFollowing/lineFollowing.cpp
+++ src/lineFollowing/lineFollowing.cpp
@@ -39,7 +39,7 @@ void LineFollowing::detect_lines(Mat &original_frame) {
     found_lines = tmp;
   }
 
-  draw_lines(original_frame, found_lines, Scalar(255, 255 , 255));
+  draw_lines(original_frame, found_lines, Scalar(255, 255, 255));
 
 }
 
@@ -60,92 +60,41 @@ void LineFollowing::close() {
 }
 
 void LineFollowing::fly() {
-  // cout << "line following\n";
-  detect_lines(control_ptr->image);
-  line_options categorization;
+  int center_x = 0 + (control_ptr->image.cols / 2);
+  int center_y = 0 + (control_ptr->image.rows / 2);
   int tolerance = 10;
-  for (int i = 0; i < found_lines.size(); ++i) {
-    float angle = abs(found_lines[i][0]);
-
-    if (angle > deg2rad(0 - tolerance) && angle < deg2rad(0 + tolerance) && categorization.vertical == Vec2f()) {
-      categorization.vertical = found_lines[i];
-    }
-    if (angle > deg2rad(90 - tolerance) && angle < deg2rad(90 + tolerance) && categorization.horizontal == Vec2f()) {
-      categorization.horizontal = found_lines[i];
-    }
-    if (angle > deg2rad(45 - tolerance) && angle < deg2rad(45 + tolerance) && categorization.sloped == Vec2f()) {
-      categorization.sloped = found_lines[i];
-      if (found_lines[i][0] < 0) {
-        categorization.d = direction::right;
-      } else if (found_lines[i][0] > 0) {
-        categorization.d = direction::left;
-      }
-    }
-
-  }
-
+  line_options categorization;
+  Point intersection_point;
 
   control_ptr->velocities.vx = 0;
   control_ptr->velocities.vy = 0;
   control_ptr->velocities.vz = 0;
   control_ptr->velocities.vr = 0;
 
-  if (found_lines.size() < 1) {
-    //I found no lines
-    // printf("Found nothing\n");
-    return;
-  }
-
-//  printf("%5.1f\n", rad2deg(found_lines[0][0]));
-  double calculated_distance = distance_from_center(found_lines[0][1], found_lines[0][0], control_ptr->image.cols,
-                                                    control_ptr->image.rows);
-
-  // printf("calc dist of %f ", calculated_distance);
-  int origin_x = 0 + (control_ptr->image.cols / 2);
-  int origin_y = 0 + (control_ptr->image.rows / 2);
-  Point pt1 = cvPoint(origin_x, origin_y);
-  Point pt2 = cvPoint(origin_x + cvRound(calculated_distance), origin_y);
-  line(control_ptr->image, pt1, pt2, Scalar(255, 255, 255), 3, CV_AA);
+  detect_lines(control_ptr->image);
 
 
-  if (found_lines.size() > 1) {
-    // I need to turn
-    //TODO: move until line is centered
-    //TODO: look for symbol, and turn
-    origin_x = 0;
-    origin_y = 0;
-    Vec2i point = find_intersection(found_lines[0], found_lines[1]);
+  if (found_lines.size() < 1) return;
 
-    line(control_ptr->image, cvPoint(origin_x + point[0] + 10, origin_y + point[1]),
-         cvPoint(origin_x + point[0] - 10, origin_y + point[1]), Scalar(255, 255, 255), 3, CV_AA);
-    line(control_ptr->image, cvPoint(origin_x + point[0], origin_y + point[1] + 10),
-         cvPoint(origin_x + point[0], origin_y + point[1] - 10), Scalar(255, 255, 255), 3, CV_AA);
-
-    if (found_lines.size() < 3) {
-      //at an intersection
-    }
-
-    if (found_lines.size() >= 3) {
-      for (int j = 0; j < (int) found_lines.size(); j++) {
-        printf("(%5.1f, %5.0f) ", rad2deg(found_lines[j][0]) + 180, found_lines[j][1]);
-      }
-      printf("\n");
-    }
-    return;
-  } else {
+  else if (found_lines.size() == 1) {
+    categorization.vertical = found_lines[0];
+    double calculated_distance = distance_from_center(categorization.vertical[1], categorization.vertical[0],
+                                                      control_ptr->image.cols, control_ptr->image.rows);
+    Point pt1 = cvPoint(center_x, center_y);
+    Point pt2 = cvPoint(center_x + cvRound(calculated_distance), center_y);
+    line(control_ptr->image, pt1, pt2, color_white, 3, CV_AA); // draw the line to the center
     // I need to snap myself to the line
-    if (found_lines[0][0] >= deg2rad(5)) {
+    if (categorization.vertical[0] >= deg2rad(5)) {
       control_ptr->velocities.vr = -.2;
       // printf("Turning Right\n");
-    } else if (found_lines[0][0] <= deg2rad(-1 * 5)) {
+    } else if (categorization.vertical[0] <= deg2rad(-1 * 5)) {
       control_ptr->velocities.vr = .2;
       // printf("Turning Left\n");
     } else {
       // printf("Checking Distance\n");
-
       double offset = calculated_distance;
       // printf("Offset is: %5.2f with a distance of %5.2f and width of %5.2f halved to %5.2f\n", offset,
-      // found_lines[0][1],
+
       // (double) control_ptr->image.cols, (control_ptr->image.cols / 2.0));
       if (-100 <= offset && offset <= 100) {
         // printf("No need to move\n");
@@ -160,8 +109,49 @@ void LineFollowing::fly() {
         // printf("Move right\n");
       }
     }
+
+  } else if (found_lines.size() == 2) {
+    //snap to a point
+    categorization.vertical = found_lines[0];
+    categorization.horizontal = found_lines[0];
+    intersection_point = find_intersection(categorization.vertical, categorization.horizontal);
+
+    //draw a cross at the intersection
+    line(control_ptr->image, cvPoint(0 + intersection_point.x + 10, 0 + intersection_point.y),
+         cvPoint(0 + intersection_point.x - 10, 0 + intersection_point.y), color_white, 3, CV_AA);
+    line(control_ptr->image, cvPoint(0 + intersection_point.x, 0 + intersection_point.y + 10),
+         cvPoint(0 + intersection_point.x, 0 + intersection_point.y - 10), color_white, 3, CV_AA);
+
+
+  } else {
+    for (int i = 0; i < found_lines.size(); ++i) {
+      float angle = abs(found_lines[i][0]);
+
+      if (angle > deg2rad(0 - tolerance) && angle < deg2rad(0 + tolerance) && categorization.vertical == Vec2f()) {
+        categorization.vertical = found_lines[i];
+      }
+      if (angle > deg2rad(90 - tolerance) && angle < deg2rad(90 + tolerance) && categorization.horizontal == Vec2f()) {
+        categorization.horizontal = found_lines[i];
+      }
+      if (angle > deg2rad(45 - tolerance) && angle < deg2rad(45 + tolerance) && categorization.sloped == Vec2f()) {
+        categorization.sloped = found_lines[i];
+        if (found_lines[i][0] < 0) {
+          categorization.d = direction::right;
+        } else if (found_lines[i][0] > 0) {
+          categorization.d = direction::left;
+        }
+      }
+
+    }
   }
 
+  if (found_lines.size() > 3) {
+    //print out the points of all 4+ lines
+    for (int j = 0; j < (int) found_lines.size(); j++) {
+      printf("(%5.1f, %5.0f) ", rad2deg(found_lines[j][0]) + 180, found_lines[j][1]);
+    }
+    printf("\n");
+  }
 
   return;
 }
